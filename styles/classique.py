312d541c30774168521d391661490d77476e0c9b
nyavorakotomavo <nyavosapp@gmail.com>
2026-08-27 11:10:01 +0200

    Create classique.py

---
+++ styles/classique.py
@@ -0,0 +1,9 @@
+from .base import Style, register
+
+register(Style(
+    nom="classique",
+    couleurs_accent=["#1a3c6e"],
+    police_titre="Helvetica-Bold",
+    en_tete_style="classique",
+    tableau_entete_degrade=False,
+))
