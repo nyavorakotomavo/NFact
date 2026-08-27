e679b79cf376bb2a660b5cf009adbb41f124ba77
nyavorakotomavo <nyavosapp@gmail.com>
2026-08-27 11:10:30 +0200

    Create stripe.py

---
+++ styles/stripe.py
@@ -0,0 +1,10 @@
+from .base import Style, register
+
+register(Style(
+    nom="stripe",
+    couleurs_accent=["#635BFF"],
+    couleur_fond_alternee="#f6f6fb",
+    police_titre="Helvetica-Bold",
+    en_tete_style="bande_couleur",
+    tableau_entete_degrade=True,
+))
